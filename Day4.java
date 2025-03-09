caa1c8ca8b763dcc134aa74010a25e042a524604
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-09 20:24:33 +0530

    learning scanner

--- Day4.java
+++ Day4.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Day4 {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        int a=10;
+        System.out.println("hey hi");
+        Scanner input= new Scanner(System.in);
+        int a=input.nextInt();
+        System.out.println(a);
     }
 
 }
