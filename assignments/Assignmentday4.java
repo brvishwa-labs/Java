d087c2e67d092158e286278451662245256e022a
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-12 19:56:46 +0530

    started assignment day4

---
+++ assignments/Assignmentday4.java
@@ -0,0 +1,44 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class Assignmentday4 {
+    public static void main(String[] args) {
+// 1)Write a program to print whether a number is even or odd, also take input from the user.
+//        Scanner sc=new Scanner(System.in);
+//        int a=sc.nextInt();
+//        if (a%2==0){
+//            System.out.println("even");
+//        }
+//        else {
+//            System.out.println("odd");
+//        }
+
+ //
+
+//// 2       Take name as input and print a greeting message for that particular name.
+//        Scanner sc=new Scanner(System.in);
+//        String a=sc.next();
+//        System.out.println("happy birthday"+" "+a);
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
