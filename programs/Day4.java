59b1950628f5e56067207f560f9bc3101557a36a
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-12 16:27:35 +0530

    done

--- programs/Day4.java
+++ programs/Day4.java
@@ -4,11 +4,37 @@ import java.util.Scanner;
 
 public class Day4 {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
-        System.out.println("hey hi");
-        Scanner input= new Scanner(System.in);
-        int a=input.nextInt();
-        System.out.println(a);
+//        System.out.println("Hello World!!");
+//        System.out.println("hey hi");
+//        Scanner input= new Scanner(System.in);
+//        int z=input.nextInt();
+//        System.out.println(z);
+
+    // type casting
+//        int a=258;
+//        byte b=(byte)(a);// 258%256=2 so output is 2
+//        System.out.println(b);
+        // automatic type promotion in exp
+//        byte u=10;
+//        byte v=20;
+//        int j=u*v;
+//        System.out.println(j);
+ //  char to number implicit
+//        int q='a';
+//        System.out.println(q);
+// while loop
+//        int c=1;
+//        while (c!=5){
+//            System.out.println(c);
+//            c++;
+//        }
+// for loop
+        for (int i = 0; i <5 ; i++) {
+            System.out.println(i);
+            
+        }
+
+
     }
 
 }
