01b265f6305994db395b0c77d44b595ceab1200d
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-13 06:39:00 +0530

    update

--- programs/Day5.java
+++ programs/Day5.java
@@ -2,6 +2,14 @@ package programs;
 
 public class Day5 {
     public static void main(String[] args) {
-
+//           finding max value
+        int a =10;
+        int b =20;
+        int c =130;
+        int max=a;
+        if (b>max)
+            max=b;
+        if(c>max) max = c;
+        System.out.println(max);
     }
 }
