fcdcfda253db251fe709f96c03dbb8412dfc7fee
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-17 10:25:58 +0530

    update

--- programs/Day5.java
+++ programs/Day5.java
@@ -1,15 +1,35 @@
 package programs;
 
+import java.util.Scanner;
+
 public class Day5 {
     public static void main(String[] args) {
 //           finding max value
-        int a =10;
-        int b =20;
-        int c =130;
-        int max=a;
-        if (b>max)
-            max=b;
-        if(c>max) max = c;
-        System.out.println(max);
+//        int a =10;
+//        int b =20;
+//        int c =130;
+//        int max=a;
+//        if (b>max)
+//            max=b;
+//        if(c>max) max = c;
+//        System.out.println(max);
+//        int max = Math.max(c,Math.max(a,b));//anathor method
+//
+//        System.out.println(max);
+        // char to number
+//
+//        Scanner sc=new Scanner(System.in);
+//        int ch=sc.next().trim().charAt(0);
+//        System.out.println(ch);
+//
+//        Scanner sc=new Scanner(System.in);
+//        char ch=sc.next().trim().charAt(0);
+//        if (ch>='a'&& ch<='z')
+//            System.out.println("lower case");
+//        else if (ch>='A'&& ch<='Z') {
+//            System.out.println("upper case");
+//        }
+// find nth fibonaci number
+        
     }
 }
