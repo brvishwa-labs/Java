effc7f965649ad46713c80405d4e6421d16de41c
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-17 21:57:48 +0530

    update

--- programs/Day5.java
+++ programs/Day5.java
@@ -30,6 +30,48 @@ public class Day5 {
 //            System.out.println("upper case");
 //        }
 // find nth fibonaci number
-        
+//Scanner sc= new Scanner(System.in);
+//int n=sc.nextInt();
+//int a=0;
+//int b=1;
+//int count=2;
+//while (count<=n){
+//    int temp=b;
+//    b=b+a;
+//    a=temp;
+//    count++;
+//
+//}
+//        System.out.println(b);
+
+//        count the number  of num in the num
+//        Scanner sc=new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int c=0;
+//        while (a>0){
+//            if(a%10==3){
+//                c++;
+//            }
+//        a=a/10;
+//        }
+//        System.out.println(c);
+
+//        reverse the number and print it
+//        Scanner sc=new Scanner(System.in);
+//        int a= sc.nextInt();
+//        int ans=0;
+//        while (a>0){
+//            int temp=a%10;
+//            a/=10;
+//            ans=ans*10+temp;
+//        }
+//        System.out.println(ans);
+
+//        CALCUlATOR PROGRAM
+        Scanner sc=new Scanner(System.in);
+
+
+
+
     }
 }
