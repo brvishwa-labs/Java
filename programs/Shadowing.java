20d3445cf580fb4a1720bedc5a2a84f76ef22d49
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-22 21:28:17 +0530

    done shadowing

--- programs/Shadowing.java
+++ programs/Shadowing.java
@@ -1,4 +1,13 @@
 package programs;
 
 public class Shadowing {
+    static int x=90;// this can be used everywhere inside this
+    public static void main(String[] args) {
+        System.out.println(x);// can be accesed here// will print 90
+        int x;//local scope
+//        System.out.println(x); this will give error because its only decllared not initialised
+        x=50;//the shadowing will be sone here like when the x is initialised again
+        System.out.println(x);// will print 50// when this is printing 50 the anathor x will be shadowed so this is called shadowing
+
+    }
 }
